c2b44be5d1fa970da335c79c08dde9f756fac597
Joseph Kratz <joseph.kratz06@gmail.com>
2018-11-17 12:28:20 -0500

    Java version working

---
+++ src/main/java/io/jkratz/katas/supermarketqueue/SuperMarketQueue.java
@@ -0,0 +1,53 @@
+package io.jkratz.katas.supermarketqueue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class SuperMarketQueue {
+
+    public static int calculateTime(int[] customerTime, int numOfTills) {
+
+        // Convert array to list to make it easier to work with
+        List<Integer> customerTimings = Arrays.stream(customerTime)
+                .boxed()
+                .collect(Collectors.toList());
+
+        // If there are no tills throw error now to avoid divide be 0 later
+        if (numOfTills < 1) {
+            throw new IllegalArgumentException("numOfTills must be at a positive integer greater than 0");
+        }
+
+        // Zero fill temporary list
+        List<Integer> q = Stream.generate(() -> 0)
+                .limit(numOfTills)
+                .collect(Collectors.toList());
+
+        for (int i: customerTimings) {
+            int min = Collections.min(q);
+            int index = q.indexOf(min);
+            int currentValue = q.get(index);
+            q.set(index, (currentValue += i));
+        }
+
+        return Collections.max(q);
+    }
+
+    private int min(int[] arr) {
+        return IntStream.range(0, arr.length)
+                .map(i -> arr[i])
+                .min()
+                .getAsInt();
+    }
+
+    private int max(int[] arr) {
+        return IntStream.range(0, arr.length)
+                .map(i -> arr[i])
+                .max()
+                .getAsInt();
+    }
+}
---
+++ src/main/kotlin/io/jkratz/katas/supermarketqueue/kotlin/SuperMarketQueue.kt
@@ -0,0 +1,8 @@
+package io.jkratz.katas.supermarketqueue.kotlin
+
+object SuperMarketQueue {
+
+    fun calculateTime(customerTime: Array<Int>, numOfTills: Int): Int {
+        return 0;
+    }
+}
