790e51eb97b31d2449ac522349bef42515655751
Joseph Kratz <joseph.kratz06@gmail.com>
2018-11-17 12:28:26 -0500

    Java version working

---
+++ src/test/java/io/jkratz/katas/supermartketqueue/SuperMarketQueueTest.java
@@ -0,0 +1,27 @@
+package io.jkratz.katas.supermartketqueue;
+
+import io.jkratz.katas.supermarketqueue.SuperMarketQueue;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SuperMarketQueueTest {
+
+    @Test
+    public void testNormalCondition() {
+        assertEquals(9, SuperMarketQueue.calculateTime(new int[] { 2, 2, 3, 3, 4, 4 }, 2));
+        assertEquals(12, SuperMarketQueue.calculateTime(new int[] {2,3,10}, 2));
+    }
+
+    @Test
+    public void testEmptyArray() {
+        assertEquals(0, SuperMarketQueue.calculateTime(new int[] {}, 1));
+    }
+
+    @Test
+    public void testSingleTillManyCustomers() {
+        assertEquals(15, SuperMarketQueue.calculateTime(new int[] { 1, 2, 3, 4, 5 }, 1));
+    }
+}
